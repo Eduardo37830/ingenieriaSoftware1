3334082f996154b9daa1fad7f9667f2d8aa52bf5
Juan Sebastian Martinez Jimenez <sebastiano805@hotmail.com>
2024-12-15 15:34:05 -0500

    Entidades de la capa de dominio

---
+++ domain/entities/medication.py
@@ -0,0 +1,8 @@
+class Medication:
+    def __init__(self, medicamento_id, name, medicationType, manufactureDate, expirationDate, quantity):
+        self.medicamento_id = medicamento_id
+        self.name = name
+        self.medicationType = medicationType
+        self.manufactureDate = manufactureDate
+        self.expirationDate = expirationDate
+        self.quantity = quantity
