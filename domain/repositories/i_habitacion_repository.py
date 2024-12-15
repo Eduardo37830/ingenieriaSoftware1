6910e644b99774f3a24cd70d93053f5c14e5c650
Eduardo Villamil <eduardo.villamil37830@ucaldas.edu.co>
2024-12-15 16:04:14 -0500

    creacion de las interfaces de repositorios

---
+++ domain/repositories/i_habitacion_repository.py
@@ -0,0 +1,25 @@
+# domain/repositories/i_habitacion_repository.py
+from abc import ABC, abstractmethod
+from typing import List
+from domain.entities.habitacion import Habitacion
+
+class IhabitacionRepository(ABC):
+    @abstractmethod
+    def save(self, habitacion: Habitacion) -> None:
+        """Guarda o actualiza una habitacion."""
+        pass
+
+    @abstractmethod
+    def find_by_id(self, habitacion_id: int) -> Habitacion:
+        """Busca una habitacion por su ID."""
+        pass
+
+    @abstractmethod
+    def find_all_by_paciente(self, paciente_id: int) -> List[Habitacion]:
+        """Obtiene todas las habitacions de un paciente."""
+        pass
+
+    @abstractmethod
+    def delete(self, habitacion_id: int) -> None:
+        """Elimina una habitacion."""
+        pass
---
+++ domain/repositories/i_proveedor_repository.py
@@ -0,0 +1,20 @@
+# domain/repositories/i_Proveedor_repository.py
+from abc import ABC, abstractmethod
+from typing import List
+from domain.entities.proveedor import Proveedor
+
+class IProveedorRepository(ABC):
+    @abstractmethod
+    def save(self, Proveedor: Proveedor) -> None:
+        """Guarda o actualiza un Proveedor."""
+        pass
+
+    @abstractmethod
+    def find_by_id(self, Proveedor_id: int) -> Proveedor:
+        """Busca un Proveedor por su ID."""
+        pass
+
+    @abstractmethod
+    def find_all(self) -> List[Proveedor]:
+        """Obtiene todos los Proveedores."""
+        pass
