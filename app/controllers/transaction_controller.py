2585ae44c5fd5dbb4438324321bdbf4c2d48011b
mauricio <tu@email.com>
2024-12-18 12:57:52 -0500

    implementaciones de las funciones de prueba en transaction_mapper

--- app/controllers/transaction_controller.py
+++ app/controllers/transaction_controller.py
@@ -1,8 +1,6 @@
 
 from flask import Blueprint, render_template, jsonify
-
-from app.mappers.transaction_mapper import TransactionMapper, SimulacionCitasAdmistrador, SimulacionCirujias
-from app.mappers.transaction_mapper import TransactionMapper
+from mappers.transaction_mapper import TransactionMapper
 
 
 # Crea un Blueprint llamado 'transactions'.
@@ -20,8 +18,6 @@ habitacionesAdmistraccion = Blueprint('habitacionesAdmistraccion', __name__)
 cirugiaAdmistraccion = Blueprint('cirugiaAdmistraccion', __name__)
 citasAdmindistraccion = Blueprint('citasAdmindistraccion', __name__)
 mapper = TransactionMapper()
-simuladorcitas = SimulacionCitasAdmistrador()
-simuladorcirugias = SimulacionCirujias()
 
 
 @transaction_blueprint.route('/transactions')
@@ -33,32 +29,36 @@ def show_admissions():
     return render_template('inicioAdministrador.html')
 @personalAdmistraccion.route('/Administrador/personalmedico')
 def show_personal():
-    return render_template('personalMedicoAdmistrador.html')
+    personal_medico= mapper.get_personal_medico()
+    return render_template('personalMedicoAdmistrador.html',personal_medico=personal_medico)
 @proveedoresAdmistraccion.route('/Administrador/proveedores')
 def show_proveedores():
-    return render_template('proveedoresAdmistrador.html')
-
-
+    proveedores= mapper.get_proveedores()
+    return render_template('proveedoresAdmistrador.html',proveedores=proveedores)
 @almacenAdmistraccion.route('/Administrador/almacen')
 def show_almacen():
     return render_template('almacenAdmistrador.html')
 @medicamentoAdmistraccion.route('/Administrador/almacen/medicamentos')
 def show_medicamento():
-    return render_template('medicamentosAdmistrador.html')
+    medicamentos= mapper.get_medicamentos()
+    return render_template('medicamentosAdmistrador.html', medicamentos=medicamentos)
 @equipoMedicoAdmistraccion.route('/Administrador/almacen/Equipo_medico')
 def show_equipomedico():
-    return render_template('equipoMedicoAdmistrador.html')
+    equiposmedicos = mapper.get_equipos_medicos()
+    return render_template('equipoMedicoAdmistrador.html', equiposmedicos=equiposmedicos)
 @pacientesAdmistraccion.route('/Administrador/pacientes')
 def show_pacientes():
-    return render_template('pacientesAdmistrador.html')
+    pacientes= mapper.get_pacientes()
+    return render_template('pacientesAdmistrador.html', pacientes=pacientes)
 @habitacionesAdmistraccion.route('/Administrador/habitaciones')
 def show_habitaciones():
-    return render_template('habitacionesAdmistrador.html')
+    habitaciones= mapper.get_habitaciones()
+    return render_template('habitacionesAdmistrador.html', habitaciones=habitaciones)
 @cirugiaAdmistraccion.route('/Administrador/cirugias')
 def show_cirugias():
-    cirugias = simuladorcirugias.get_cirugias()
+    cirugias = mapper.get_cirugias()
     return render_template('cirugiasAdmistrador.html', cirugias=cirugias)
 @citasAdmindistraccion.route('/Administrador/citas')
 def show_citas():
-    citas = simuladorcitas.get_citas()
+    citas = mapper.get_citas()
     return render_template('citasAdmistrador.html', citas=citas)
